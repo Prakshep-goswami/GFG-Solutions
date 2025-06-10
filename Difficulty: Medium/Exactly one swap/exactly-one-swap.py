de3abb37615760b4c06080edf1a5cb11ca15fc56
PRAKSHEP GOSWAMI <prakshepgoswami@gmail.com>
2025-06-10 18:24:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def countStrings(self, s): 
+        n = len(s)
+        freq = Counter(s)
+        total_swaps = n * (n - 1) // 2
+        redundant = sum(f * (f - 1) // 2 for f in freq.values())
+        res = total_swaps - redundant
+        if redundant > 0:
+            return res + 1
+        return res
+ 
+

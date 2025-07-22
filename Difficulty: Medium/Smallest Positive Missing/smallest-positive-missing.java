300748dc2b5f1c9620fad93720819e950bcf9a14
PRAKSHEP GOSWAMI <prakshepgoswami@gmail.com>
2025-07-22 22:16:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+       int i=1;
+        for(int val : arr){
+            set.add(val);
+        }
+       while(!set.isEmpty()){
+           if(!set.contains(i)){
+               return i;
+           }
+           i++;
+       }
+        return -1;
+    }
+}

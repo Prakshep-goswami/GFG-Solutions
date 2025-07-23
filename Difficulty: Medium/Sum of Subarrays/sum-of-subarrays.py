e7ce3462bfa18f02ad085005c57bb27cdaa3c906
PRAKSHEP GOSWAMI <prakshepgoswami@gmail.com>
2025-07-23 23:29:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Subarrays/sum-of-subarrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, arr):
+        total = 0
+        prev = 0
+        for i in arr:
+            total += prev + i
+            prev += i
+            
+        n = len(arr)
+        total *= n
+        i = 0
+        while n and i < len(arr):
+            total -= (arr[i] * n * (n - 1))
+            n -= 1
+            i += 1
+            
+        return total
